670b8f59b2aacac01592166dfb887db15696197b
EvilNotAl0ne <sacha1234sacha@mail.ru>
2024-10-13 14:23:25 +0300

    settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--a*x8vsk3b&vdr8*p9_@c3w9)0s0h$wv@urz40uztgrglozf1y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
